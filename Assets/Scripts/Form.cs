0744188a37701977acac5c75af74124d265ed4f1
Evgeny Khoroshavin <themplarer@gmail.com>
2021-04-15 23:40:20 +0500

    added test controls

--- Assets/Scripts/Form.cs
+++ Assets/Scripts/Form.cs
@@ -2,23 +2,54 @@ using System;
 using System.IO;
 using UnityEngine;
 
-public class Form
+public class Form : MonoBehaviour
+{
+    private FormCard curForm;
+
+    private void Awake()
+    {
+        curForm = FormCard.GenerateForm();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+            ChangeFormCard();
+    }
+
+    private void ChangeFormCard() => ChangeFormCard(FormCard.GenerateForm());
+
+    //TODO: change controls & add like/dislike processing
+    private void ChangeFormCard(FormCard newForm)
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            Debug.Log($"You liked {curForm.Name}, {curForm.Age}");
+        else Debug.Log($"You disliked {curForm.Name}, {curForm.Age}");
+
+        curForm = newForm;
+    }
+}
+
+public class FormCard
 {
     public string Name;
     public int Age;
     public Sex Sex;
     public string Description;
+    public int ID;
+    public bool IsSpecial;
     //TODO: generate and store images
 
-    public Form(string name, int age, Sex sex, string desc)
+    public FormCard(string name, int age, Sex sex, string desc, bool isSpecial = false)
     {
         Name = name;
         Age = age;
         Sex = sex;
         Description = desc;
+        IsSpecial = isSpecial;
     }
 
-    public static Form GenerateForm()
+    public static FormCard GenerateForm()
     {
         var random = new System.Random();
         var age = random.Next(18, 45);
@@ -26,7 +57,7 @@ public class Form
         var path = @"Assets\Forms\" + sex.ToString() + "Names.txt";
         var names = File.ReadAllLines(path);
 
-        return new Form(names[random.Next(names.Length)],
+        return new FormCard(names[random.Next(names.Length)],
             age, sex, "");
         //TODO: generate description (using pregenerated info?)
     }
--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -7,15 +7,6 @@ public class Scale : MonoBehaviour
     private static int scaleSize = 20;
     public int scaleValue = 10;
 
-    // временно для отладки
-    void Awake()
-    {
-        var form = Form.GenerateForm();
-        Debug.Log(form.Name);
-        Debug.Log(form.Age);
-        Debug.Log(form.Sex);
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -25,8 +16,7 @@ public class Scale : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var dt = 1;
-        if (true) scaleValue += dt;
-
+        //var dt = 1;
+        //if (true) scaleValue += dt;
     }
 }
