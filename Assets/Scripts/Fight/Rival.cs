0efc3d2c4f37eba3d36cc198920711cc8cac4b33
Maria <fufeffuf@gmail.com>
2021-04-17 08:09:50 +0500

    added screen

--- Assets/Scripts/Fight/Rival.cs
+++ Assets/Scripts/Fight/Rival.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rival : MonoBehaviour
 {
+    private int lives = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,8 @@ public class Rival : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (lives == 0)
+            Debug.Log("You win!");
         
     }
 }
