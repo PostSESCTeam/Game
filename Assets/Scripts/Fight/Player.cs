a6968c6af1c69f1862ccea78215b5bd0d887373b
Maria <fufeffuf@gmail.com>
2021-04-28 14:48:02 +0500

    act

--- Assets/Scripts/Fight/Player.cs
+++ Assets/Scripts/Fight/Player.cs
@@ -5,25 +5,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Vector2 position;
+    private const float stepSize = 0.1f;
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer sprite;
+    private float speed = 5.0f;
+    private Bullet bullet;
 
     void Start()
     {
-        position = new Vector2();
+        rigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        bullet = Resources.Load<Bullet>("Bullet");
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Horizontal"))
+        if (Input.GetButton("Horizontal") && Input.GetButton("Vertical"))
         {
-            position += new Vector2(Input.GetAxis("Horizontal") > 0 ? 1 : -1, 0);
-        } 
-        else if (Input.GetButtonDown("Vertical"))
-        {
-            position += new Vector2(0, Input.GetAxis("Vertical") > 0 ? 1 : -1);
-        } else if (Input.GetKeyDown("Fire"))
-        {
-            // TODO: ye
+            Run(new Vector3(Input.GetAxis("Horizontal") > 0 ? stepSize : -stepSize, Input.GetAxis("Vertical") > 0 ? stepSize : -stepSize));
         }
+        else if (Input.GetButton("Horizontal"))
+        {
+            Run(new Vector3(Input.GetAxis("Horizontal") > 0 ? stepSize : -stepSize, 0));
+            sprite.flipX = Input.GetAxis("Horizontal") < 0.0f;
+        } 
+        else if (Input.GetButton("Vertical"))
+        {
+
+            Run(new Vector3(0, Input.GetAxis("Vertical") > 0 ? stepSize : -stepSize));
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    public void Run(Vector3 direction)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+
+    public void Shoot()
+    {
+        Vector3 position = transform.position;
+        position.y += 0.1f;
+        Instantiate(bullet, position, bullet.transform.rotation);
     }
 }
