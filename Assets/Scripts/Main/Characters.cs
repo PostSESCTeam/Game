3eae934aac2d061b2c0feb11c1872f9d1815bbdd
Maria <fufeffuf@gmail.com>
2021-05-27 15:54:11 +0500

    added text

--- Assets/Scripts/Main/Characters.cs
+++ Assets/Scripts/Main/Characters.cs
@@ -11,15 +11,15 @@ public class Characters
     {
         plotCards = new List<FormCard>(new FormCard[]
         {
-            //CreatePlotCharacter("Виктор", 433, Sex.Male, "Не кровосос, а дегустатор", 1.0, "Victor"),
+            CreatePlotCharacter("Виктор", 433, Sex.Male, "Не кровосос, а дегустатор", 1.0, "Victor"),
             //CreatePlotCharacter("Фуршет", 12, Sex.Female, "Невеста божья", 1.0, "Fufe"),
             //CreatePlotCharacter("Марк", 129, Sex.Male, "Что-то по рептилоидному", 1.0, "Reptiloid", "Teleport"),
-            //CreatePlotCharacter("Тварь", 37, Sex.Male, "[ДАННЫЕ УДАЛЕНЫ]", 1.0, "Tvar", "Crazy"),
+            CreatePlotCharacter("Тварь", 37, Sex.Male, "[ДАННЫЕ УДАЛЕНЫ]", 1.0, "Tvar", "Crazy"),
             //CreatePlotCharacter("Лидия", 19, Sex.Female, "Колюсь", 1.0, "Lydia"),
             //CreatePlotCharacter("Эрик", 61, Sex.Male, "Спой мне, и я стану твоим личным Ангелом", 1.0, "Eric"),
-            //CreatePlotCharacter("Алексей", 19, Sex.Male, "Ходит дурачок по лесу, ищет дурачок глупее себя", 1.0, "Alexey", "Crazy"),
+            CreatePlotCharacter("Алексей", 19, Sex.Male, "Ходит дурачок по лесу, ищет дурачок глупее себя", 1.0, "Alexey", "Crazy"),
             //CreatePlotCharacter("Дед", 73, Sex.Male, "К запаху привыкнешь", 1.0, "Ded"),
-            //CreatePlotCharacter("Константин", 19, Sex.Male, "заведу роман с твоим скелетом в шкафу", 1.0, "Skelet"),
+            CreatePlotCharacter("Константин", 19, Sex.Male, "заведу роман с твоим скелетом в шкафу", 1.0, "Skelet"),
             //CreatePlotCharacter("Игорь", 15, Sex.Male, "зацени прессак", 1.0, "Igor"),
             //CreatePlotCharacter("Иннокентий", 16, Sex.Male, "Рогатым рогов не наставить!", 1.0, "Kesha"),
             //CreatePlotCharacter("Леня", 16, Sex.Male, "Я с незнакомыми не знакомлюсь!!!", 1.0, "Leonid"),
